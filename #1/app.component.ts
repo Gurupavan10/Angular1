93341f7970355793ec06ee22eb3b3a16686b6cbd
Gurupavan10 <103406363+Gurupavan10@users.noreply.github.com>
2022-04-26 12:26:18 +0530

    Add files via upload

---
+++ #1/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+
+	users = [{name:'guru',age:20},{name:'pavan',age:22},{name:'jaggu',age:30}];
+
+	
+ }
+
